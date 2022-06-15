b5c9f20d9449135adc4af9d4296b00ed4588695b
Lucas Rocha <santoslucas.dev@gmail.com>
2022-06-15 17:29:45 -0300

    Criando models e migrations de produto

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,31 @@
+from distutils.command.upload import upload
 from django.db import models
 
-# Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao_curta = models.TextField(max_length=255)
+    descricao_longa = models.TextField()
+    imagem = models.ImageField(
+        upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    preco_marketing = models.FloatField()
+    preco_marketing_promocional = models.FloatField(default=0)
+    tipo = models.CharField(
+        default='V',
+        max_length=1,
+        choices=(
+            ('V', 'Variação'),
+            ('S', 'Simples'),
+        )
+    )
+
+
+"""
+        Variacao:
+            nome - char
+            produto - FK Produto
+            preco - Float
+            preco_promocional - Float
+            estoque - Int
+"""
